ed70b1e7d9947bdf2403389f9f934ff1236608d2
Annie McElroy <134727217+Annie-McElroy@users.noreply.github.com>
2023-07-14 16:58:46 -0500

    Began pseudo coding for JS, pulled current work from colton branch

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+// Index Homepage: Variables
+    // Start Quiz button (for both show and movies - focus on shows for now)
+    // add event listener to direct to quiz page
+
+// Quiz page Variables
+    // Questions: array with objects in it that include questions and choices
+        // Answers array with show IDs
+    // Select choices buttons with querySelector and function event to direct to next question
+    // Function to display questions and choices
+        // 
+    // Variables for API Keys - GoogleBook API and TMBD
+    // Empty array to push book ISBN based on selected shows
+    // For each show ID needs an array showing it's 5 book ISBN (25 variables)
+    // Math.random to return 5 ISBN options to the results page
+        // Call function for GoogleBooks API show the thumbnails
+
+
+
+// Results
+    // Array for recommendations to display books in box
+    // 
