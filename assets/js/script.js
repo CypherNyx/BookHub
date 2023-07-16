fa99990c2f7378de659eca734a077b4f33458f3a
Annie McElroy <134727217+Annie-McElroy@users.noreply.github.com>
2023-07-16 09:40:36 -0500

    Added variables for gb apikey, placement variables for questions and options, and began writing the varilable for the book groups

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,56 @@
 // Index Homepage: Variables
+// Possible make variables for the navigation bar to redirect?
     // Start Quiz button (for both show and movies - focus on shows for now)
-var startShowBtn = document.querySelector(".showBinger")
-var startMovieBtn = document.querySelector(".movieFan")
+var startShowBtn = document.querySelector(".showBinger");
+var startMovieBtn = document.querySelector(".movieFan");
 
     // add event listener to direct to quiz page
-    // Possible make variables for the navigation bar to redirect?
 
 // Quiz page Variables
+    // Variables for API Keys - GoogleBook API and TMBD
+var googleBooksKey = "AIzaSyCtpFU2A6HSKklQfEKS0tabr54FI8fyQjc";
+var imdbKey = "";
     // Questions: array with objects in it that include questions and choices
-        // Answers array with show IDs
+    // Empty array to push book ISBN based on selected shows
+var userResults = [];
+    // Answers array with show IDs
+var questions [
+    {
+        questionText = "";
+        answerOptions =  "";
+    }
+    {
+        questionText = "";
+        answerOptions =  "";
+    }
+    {
+        questionText = "";
+        answerOptions =  "";
+    }
+    {
+        questionText = "";
+        answerOptions =  "";
+    }
+    {
+        questionText = "";
+        answerOptions =  "";
+    }
+]
+
+    // For each show ID needs an array showing it's 5 book ISBN (25 variables) - Might be better to put the books into groups and create if statements for each answer?
+
+var bookGroups = [
+    {
+        // The Witcher
+        group: 1,
+        showID: "tt5180504",
+        bookIDs: ["9780316438988", "9781649374172", "9780316212298", "9780062564566", "9780316246279"]
+    }
+]
+
     // Select choices buttons with querySelector and function event to direct to next question
     // Function to display questions and choices
         // Next button click event (if else statement to make sure they've selected something)
-    // Variables for API Keys - GoogleBook API and TMBD
-    // Empty array to push book ISBN based on selected shows
-    // For each show ID needs an array showing it's 5 book ISBN (25 variables)
     // Math.random to return 5 ISBN options to the results page
         // Call function for GoogleBooks API show the thumbnails
 
