bd0438ee0f031c3a1eed47ea9eae3cacd9202158
Annie McElroy <134727217+Annie-McElroy@users.noreply.github.com>
2023-07-16 15:54:20 -0500

    Fixed syntax of both array objects and added in the rest of the current book IDs with show IDs as well as show titles to keep track

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -10,28 +10,28 @@
         // Empty array to push book ISBN based on selected shows
     var userResults = [];
         // Answers array with show IDs
-    var questions [
+    var questions = [
         {
-            questionText = "Let's say you were having a really awful day, which of these shows would you watch?",
-            answerOptions =  "",
+            questionText: "Let's say you were having a really awful day, which of these shows would you watch?",
+            answerOptions:  "",
         },
         {
-            questionText = "If you were bored and had nothing to do, which show would you go to first?",
-            answerOptions = "",
+            questionText: "If you were bored and had nothing to do, which show would you go to first?",
+            answerOptions: "",
         },
         {
-            questionText = "Which of these shows would you always recommend to a friend?",
-            answerOptions = "",
+            questionText: "Which of these shows would you always recommend to a friend?",
+            answerOptions: "",
         },
         {
-            questionText = "Which of these shows would you recommend to a complete stranger?",
-            answerOptions = "",
+            questionText: "Which of these shows would you recommend to a complete stranger?",
+            answerOptions: "",
         },
         {
-            questionText = "Last but not least, out of these 5 shows based on books, pick one that you've loved reading or want to read.",
-            answerOptions = "",
+            questionText: "Last but not least, out of these 5 shows based on books, pick one that you've loved reading or want to read.",
+            answerOptions: "",
         }
-    ]
+    ];
     
         // For each show ID needs an array showing it's 5 book ISBN (25 variables) - Might be better to put the books into groups and create if statements for each answer?
     
@@ -41,62 +41,70 @@
             group: 1,
             title: "The Witcher",
             showID: "tt5180504",
-            bookIDs: ["9780316438988", "9781649374172", "9780316212298", "9780062564566", "9780316246279"]
-        }
+            bookIDs: ["9780316438988", "9781649374172", "9780316212298", "9780062564566", "9780316246279"],
+        },
         {
             // LOTR: The Rings of Power
             group: 2,
             title: "Lord of the Rings: The Rings of Power",
             showID: "tt7631058",
-            bookIDs: ["9780007264896", "9780008376062", "9780593237649", "9781423136149", "9780547851396"]
-        }
+            bookIDs: ["9780007264896", "9780008376062", "9780593237649", "9781423136149", "9780547851396"],
+        },
         {
             // Narcos
             group: 3,
+            title: "Narcos",
             showID: "tt2707408",
-            bookIDs: ["9781537296302", "9780393336641", "9781912885039", "9780993021596", "9781548433536"]
-        }
+            bookIDs: ["9781537296302", "9780393336641", "9781912885039", "9780993021596", "9781548433536"],
+        },
         {
             // Bridgerton
             group: 4,
+            title: "Bridgerton",
             showID: "tt8740790",
-            bookIDs: ["9780063078901", "9780593438152", "9781922733337", "9781984805683", "9781420155235"]
-        }
+            bookIDs: ["9780063078901", "9780593438152", "9781922733337", "9781984805683", "9781420155235"],
+        },
         {
             // The Good Place
             group: 5,
+            title: "The Good Place",
             showID: "tt4955642",
-            bookIDs: ["9780307476531", "9780593722909", "9781982167363", "9780393341768", "9781451626650"]
-        }
+            bookIDs: ["9780307476531", "9780593722909", "9781982167363", "9780393341768", "9781451626650"],
+        },
         {
             // True Blood
             group: 6,
+            title: "True Blood",
             showID: "tt0844441",
-            bookIDs: ["9780441008537", "9781101619902", "9780575129481", "9780593335154", "9780061742095"]
-        }
+            bookIDs: ["9780441008537", "9781101619902", "9780575129481", "9780593335154", "9780061742095"],
+        },
         {
-            // The Witcher
-            group: 1,
-            showID: "",
-            bookIDs: ["", "", "", "", ""]
-        }
+            // American Horror Story
+            group: 7,
+            title: "American Horror Story",
+            showID: "tt1844624",
+            bookIDs: ["9781639361205", "9781637896075", "9781941147177", "9781639361205", "9781501142970"],
+        },
         {
-            // The Witcher
-            group: 1,
-            showID: "",
-            bookIDs: ["", "", "", "", ""]
-        }
+            // Jack Ryan
+            group: 8,
+            title: "Jack Ryan",
+            showID: "tt5057054",
+            bookIDs: ["9780425248607", "9780525538431", "9781101603758", "9780451413192", "9780515153651"],
+        },
         {
-            // The Witcher
-            group: 1,
-            showID: "",
-            bookIDs: ["", "", "", "", ""]
-        }
+            // Lost
+            group: 9,
+            title: "Lost",
+            showID: "tt0411008",
+            bookIDs: ["9780374104092", "9780425278901", "9780316449052", "9781481440882", "9780358211587"],
+        },
         {
-            // The Witcher
-            group: 1,
-            showID: "",
-            bookIDs: ["", "", "", "", ""]
+            // My. Robot
+            group: 10,
+            title: "Mr. Robot",
+            showID: "tt4158110",
+            bookIDs: ["9780307454546", "9781477828496", "9781612195216", "9780441007462", "9780996066624"]
         }
     ]
     
