62acba344bb4cdb58311efaad50fe786a94c18f3
Annie McElroy <134727217+Annie-McElroy@users.noreply.github.com>
2023-07-16 09:50:20 -0500

    Started added bookIDs with showIDs into group objects within one array

--- assets/js/script.js
+++ assets/js/script.js
@@ -46,6 +46,60 @@ var bookGroups = [
         showID: "tt5180504",
         bookIDs: ["9780316438988", "9781649374172", "9780316212298", "9780062564566", "9780316246279"]
     }
+    {
+        // LOTR: The Rings of Power
+        group: 2,
+        showID: "tt7631058",
+        bookIDs: ["9780007264896", "9780008376062", "9780593237649", "9781423136149", "9780547851396"]
+    }
+    {
+        // Narcos
+        group: 3,
+        showID: "tt2707408",
+        bookIDs: ["9781537296302", "9780393336641", "9781912885039", "9780993021596", "9781548433536"]
+    }
+    {
+        // Bridgerton
+        group: 4,
+        showID: "tt8740790",
+        bookIDs: ["9780063078901", "9780593438152", "9781922733337", "9781984805683", "9781420155235"]
+    }
+    {
+        // The Good Place
+        group: 5,
+        showID: "tt4955642",
+        bookIDs: ["9780307476531", "9780593722909", "9781982167363", "9780393341768", "9781451626650"]
+    }
+    {
+        // True Blood
+        group: 6,
+        showID: "tt0844441",
+        bookIDs: ["9780441008537", "9781101619902", "9780575129481", "9780593335154", "9780061742095"]
+    }
+    {
+        // The Witcher
+        group: 1,
+        showID: "",
+        bookIDs: ["", "", "", "", ""]
+    }
+    {
+        // The Witcher
+        group: 1,
+        showID: "",
+        bookIDs: ["", "", "", "", ""]
+    }
+    {
+        // The Witcher
+        group: 1,
+        showID: "",
+        bookIDs: ["", "", "", "", ""]
+    }
+    {
+        // The Witcher
+        group: 1,
+        showID: "",
+        bookIDs: ["", "", "", "", ""]
+    }
 ]
 
     // Select choices buttons with querySelector and function event to direct to next question
