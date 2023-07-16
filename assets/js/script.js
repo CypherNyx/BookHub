1935d3728be6895f5e07e207ae7bb36ed67c0bdb
Annie McElroy <134727217+Annie-McElroy@users.noreply.github.com>
2023-07-16 09:27:04 -0500

    Added additional pseudo coding and possible requirements, added variables for start buttons for game/quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,17 @@
 // Index Homepage: Variables
     // Start Quiz button (for both show and movies - focus on shows for now)
+var startShowBtn = document.querySelector(".showBinger")
+var startMovieBtn = document.querySelector(".movieFan")
+
     // add event listener to direct to quiz page
+    // Possible make variables for the navigation bar to redirect?
 
 // Quiz page Variables
     // Questions: array with objects in it that include questions and choices
         // Answers array with show IDs
     // Select choices buttons with querySelector and function event to direct to next question
     // Function to display questions and choices
-        // 
+        // Next button click event (if else statement to make sure they've selected something)
     // Variables for API Keys - GoogleBook API and TMBD
     // Empty array to push book ISBN based on selected shows
     // For each show ID needs an array showing it's 5 book ISBN (25 variables)
@@ -17,5 +21,5 @@
 
 
 // Results
-    // Array for recommendations to display books in box
+    // Array for recommendations to display books in box comes from empty array with pushed arrays into it (concat rather than push if both arrays, push if book IDs are only strings)
     // 
