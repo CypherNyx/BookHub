9f19ba901ef0d15c8940c208ce44743e448e4c56
Dahlia Guido <dguido.dev@gmail.com>
2023-07-16 23:03:56 -0500

    debuging TMDB API

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,6 @@
 // Index Homepage: Variables
 // NavBar Clickable: make variables for the navigation bar to redirect? - Dahlia -done
 
-// Start Quiz button (for both show and movies - focus on shows for now)
-var startShowBtn = document.querySelector(".showBinger");
-var startMovieBtn = document.querySelector(".movieFan");
-
 //vars for main (top) nav items
 var navHomeBtn = document.getElementById("homeBtn");
 var navExploreBtn = document.getElementById("explore");
@@ -16,6 +12,19 @@ var navBestSellersBtn = document.getElementById("bestSellers");
 var footerNavExplore = document.getElementById("fExplore");
 var footerNavBookshelf = document.getElementById("fBookshelf");
 var footerNavBestSellers = document.getElementById("fBestSellers");
+
+// Start Quiz button (for both show and movies - focus on shows for now)
+var startShowBtn = document.querySelector(".showBinger");
+var startMovieBtn = document.querySelector(".movieFan");
+
+//vars for featured show and book in Homepage
+var featuredSHOW = document.getElementById("FeaturedSHOWImg");
+var featuredBOOK = document.getElementById("FeaturedBOOKImg");
+
+// Variables for API Keys - GoogleBook API and TMBD
+var googleBooksKey = "AIzaSyCtpFU2A6HSKklQfEKS0tabr54FI8fyQjc";
+var tmdbKey = "&api_key=1866aca8e0c0dfcbef153230d45d9a50";
+
 
 // Event listeners for Main navbar - DG
 navHomeBtn.addEventListener('click', () => {
@@ -88,3 +97,31 @@ footerNavBestSellers.addEventListener('click', () => {
             window.location.href ='quiz.html';
         }
 });
+
+// Set Featured Show Image (featuredSHOW) (featuredBOOK)
+
+// document.getElementById('poster').src = 'https://image.tmdb.org/t/p/w185' + poster_path;
+
+var showInfo = {
+    show: 'tt4574334',
+    fetchInfo: function (show) {
+      fetch(
+        'https://api.themoviedb.org/3/find/' + this.show + '?external_source=imdb_id' + tmdbKey
+      )
+        .then((response) => response.json())
+        .then((data) => console.log(data));
+
+    },
+    /*this.displayshowInfo(data));
+    displayshowInfo: function (data) {
+      //const { name } = data.tv_results;
+      const { original_name, media_type, overview, poster_path } = data.tv_results[0];
+      console.log('name:', original_name, 'media ', media_type, 'details: ', overview);
+      console.log(poster_path);
+      featuredSHOW.src = 'https://image.tmdb.org/t/p/w185' + poster_path;
+    //   document.getElementById('showName').innerHTML = original_name;
+      
+
+      
+    },*/
+  };
