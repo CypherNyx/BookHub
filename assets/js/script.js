a615dc60dae8bcab6466be69df4ba79c29450f0f
Dahlia Guido <dguido.dev@gmail.com>
2023-07-16 21:07:59 -0500

    making footer navbar clickable

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,21 @@
 // Should be on every single HTML for the clickable navbar to work
 // Index Homepage: Variables
-// NavBar Clickable: make variables for the navigation bar to redirect? - Dahlia
+// NavBar Clickable: make variables for the navigation bar to redirect? - Dahlia -done
 
 // Start Quiz button (for both show and movies - focus on shows for now)
 var startShowBtn = document.querySelector(".showBinger");
 var startMovieBtn = document.querySelector(".movieFan");
+
+//vars for main (top) nav items
 var navHomeBtn = document.getElementById("homeBtn");
 var navExploreBtn = document.getElementById("explore");
 var navBookshelfBtn = document.getElementById("bookshelf");
 var navBestSellersBtn = document.getElementById("bestSellers");
+
+// vars for footer nav items
+var footerNavExplore = document.getElementById("fExplore");
+var footerNavBookshelf = document.getElementById("fBookshelf");
+var footerNavBestSellers = document.getElementById("fBestSellers");
 
 // Event listeners for Main navbar - DG
 navHomeBtn.addEventListener('click', () => {
@@ -33,6 +40,7 @@ navBookshelfBtn.addEventListener('click', () => {
     window.location.href ='bookshelf.html';
 }
 });
+
 // TODO: Replace link -Best sellers page is still a MAYBE for now?
 navBestSellersBtn.addEventListener('click', () => {
     
@@ -45,7 +53,6 @@ navBestSellersBtn.addEventListener('click', () => {
 
 
 // make quiz buttons clickable
-
 startShowBtn.addEventListener('click', ()=> {
     window.location.href ='./assets/pages/quiz.html';
 });
@@ -55,3 +62,29 @@ startMovieBtn.addEventListener('click', ()=> {
     window.location.href ='./assets/pages/quiz.html';
 });
 
+
+// add event listeners for footer nav items
+footerNavExplore.addEventListener('click', () => {
+    if (window.location.href.endsWith('index.html')) {
+    window.location.href ='./assets/pages/explore.html';
+    } else {
+        window.location.href ='explore.html';
+    }
+});
+footerNavBookshelf.addEventListener('click', () => {
+    if (window.location.href.endsWith('index.html')) {
+    window.location.href ='./assets/pages/bookshelf.html';
+} else {
+    window.location.href ='bookshelf.html';
+}
+});
+
+// TODO: Replace link -Best sellers 
+footerNavBestSellers.addEventListener('click', () => {
+    
+    if (window.location.href.endsWith('index.html')) {
+        window.location.href ='./assets/pages/quiz.html';
+        } else {
+            window.location.href ='quiz.html';
+        }
+});
