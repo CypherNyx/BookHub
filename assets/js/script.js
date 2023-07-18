9c4f12a0f1d1e66cbe340dcd38c2f99bdf8ed843
Dahlia Guido <dguido.dev@gmail.com>
2023-07-18 17:38:12 -0500

    adding Colton's GBook API key

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,7 +22,7 @@ var featuredSHOW = document.getElementById("FeaturedSHOWImg");
 var featuredBOOK = document.getElementById("FeaturedBOOKImg");
 
 // Variables for API Keys - GoogleBook API and TMBD
-var googleBooksKey = "AIzaSyCtpFU2A6HSKklQfEKS0tabr54FI8fyQjc";
+var googleBooksKey = "AIzaSyBSGiO1LqCBsdlWOKX20F6c1qg4EYyLWgA";
 var tmdbKey = "&api_key=1866aca8e0c0dfcbef153230d45d9a50";
 
 
